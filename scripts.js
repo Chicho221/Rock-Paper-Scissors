dc20c712b3bd0a7a4ebb87a193ad6c0aa7ad5dc9
Chicho221 <135489598+Chicho221@users.noreply.github.com>
2023-09-10 02:10:51 +0200

    Add function game that repeats itself 5 times.

--- scripts.js
+++ scripts.js
@@ -1,21 +1,24 @@
-//Prompt for user to enter his choice
 let playerChoice;
+let computerChoice;
+let scorePlayer = 0;
+let scoreComputer = 0;
+//Prompt for user to enter his choice
 function getPlayerChoice(){
     playerChoice = prompt("Choose: Rock, Paper, Scissors.");
+    playerChoice = playerChoice.toLowerCase();
 }
 //Tell computer to choose random number from 1 to 3 and change it into a string
-let computerChoice = 0;
 function getComputerChoice(){
     computerChoice = Math.floor(Math.random() * 3 ) + 1;
     switch(computerChoice){
         case 1:
-            computerChoice = "Rock";
+            computerChoice = "rock";
         break;
         case 2:
-            computerChoice = "Paper";
+            computerChoice = "paper";
         break;
         case 3:
-            computerChoice = "Scissors";
+            computerChoice = "scissors";
         break;
     }
 }
@@ -24,46 +27,74 @@ function playRound(){
     getPlayerChoice();
     getComputerChoice();
 //Check IF player choice is rock
-    if(playerChoice === "Rock"){
+    if(playerChoice === "rock"){
 //Check what computer picked
         switch(computerChoice){
-            case "Rock":
+            case "rock":
                 alert("Looks like a draw!");
+                scoreComputer++;
+                scorePlayer++;
             break;
-            case "Paper":
+            case "paper":
                 alert("Looks that you lost.");
+                scoreComputer++;
             break;
-            case "Scissors":
+            case "scissors":
                 alert("Looks like you won!");
+                scorePlayer++;
             break;
         }
 //Check IF player choice is paper
-    } else if(playerChoice === "Paper"){
+    } else if(playerChoice === "paper"){
 //Check what computer picked
         switch(computerChoice){
-            case "Rock":
+            case "paper":
                 alert("Looks like a draw!");
+                scoreComputer++;
+                scorePlayer++;
             break;
-            case "Paper":
+            case "scissors":
                 alert("Looks that you lost.");
+                scoreComputer++;
             break;
-            case "Scissors":
+            case "rock":
                 alert("Looks like you won!");
+                scorePlayer++;
             break;
         }
 //Check IF player choice is scissors
     }else{
 //Check what computer picked
         switch(computerChoice){
-            case "Rock":
+            case "scissors":
                 alert("Looks like a draw!");
+                scoreComputer++;
+                scorePlayer++;
             break;
-            case "Paper":
+            case "rock":
                 alert("Looks that you lost.");
+                scoreComputer++;
             break;
-            case "Scissors":
+            case "paper":
                 alert("Looks like you won!");
+                scorePlayer++;
             break;
         }
     }
+}
+//Create function that plays 5 times, keeps a score and reports a winner
+
+function game(){
+    resetScore()
+    for (let i = 0;i <= 4; i++) {
+        playRound();
+    }
+    alert("Player " + scorePlayer + ":" + scoreComputer + " Computer");
+    console.log("Player " + scorePlayer + ":" + scoreComputer + " Computer");
+}
+//Resets the score
+
+function resetScore(){
+    scoreComputer = 0;
+    scorePlayer = 0;
 }
